6b7560ae1de0e0fec1f52992e35dee023a91564b
barakaganai <barakaganai01@gmail.com>
2023-06-10 21:03:49 +0300

    import components to the home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,18 @@
+import About from "../../components/about/About";
+import Header from "../../components/header/Header";
+import Leader from "../../components/leader/Leader";
 import NavBar from "../../components/navBar/NavBar";
+import Profile from "../../components/profile/Profile";
 import "./Home.scss";
 
 function Home() {
   return (
     <div>
       <NavBar />
+      <Header />
+      <About />
+      <Leader />
+      <Profile />
     </div>
   );
 }
