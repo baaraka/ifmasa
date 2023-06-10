225db73b0549862d4566b7e4033ed4019731d9c6
barakaganai <barakaganai01@gmail.com>
2023-06-10 21:06:45 +0300

    create profile.jsx

---
+++ src/components/profile/Profile.jsx
@@ -0,0 +1,70 @@
+import "./Profile.scss";
+
+function Profile() {
+  return (
+    <div className="profile">
+      <div className="profileContainer">
+        <h1>IFMASA MEMBER'S PROFILE</h1>
+        <div className="pContainer">
+          <div className="pContainerList">
+            <img
+              src="https://images.pexels.com/photos/4560083/pexels-photo-4560083.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+              alt=""
+            />
+            <span>name: Baraka Ganai</span>
+            <span>Education: Bachelor degree in information technology </span>
+            <span>Research Interested: Web development</span>
+          </div>
+          <div className="pContainerList">
+            <img
+              src="https://images.pexels.com/photos/4560083/pexels-photo-4560083.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+              alt=""
+            />
+            <span>name: Baraka Ganai</span>
+            <span>Education: Bachelor degree in information technology </span>
+            <span>Research Interested: Web development</span>
+          </div>
+          <div className="pContainerList">
+            <img
+              src="https://images.pexels.com/photos/4560083/pexels-photo-4560083.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+              alt=""
+            />
+            <span>name: Baraka Ganai</span>
+            <span>Education: Bachelor degree in information technology </span>
+            <span>Research Interested: Web development</span>
+          </div>
+          <div className="pContainerList">
+            <img
+              src="https://images.pexels.com/photos/4560083/pexels-photo-4560083.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+              alt=""
+            />
+            <span>name: Baraka Ganai</span>
+            <span>Education: Bachelor degree in information technology </span>
+            <span>Research Interested: Web development</span>
+          </div>
+          <div className="pContainerList">
+            <img
+              src="https://images.pexels.com/photos/4560083/pexels-photo-4560083.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+              alt=""
+            />
+            <span>name: Baraka Ganai</span>
+            <span>Education: Bachelor degree in information technology </span>
+            <span>Research Interested: Web development</span>
+          </div>
+          <div className="pContainerList">
+            <img
+              src="https://images.pexels.com/photos/4560083/pexels-photo-4560083.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+              alt=""
+            />
+            <span>name: Baraka Ganai</span>
+            <span>Education: Bachelor degree in information technology </span>
+            <span>Research Interested: Web development</span>
+          </div>
+        </div>
+        <button>VIEW MORE</button>
+      </div>
+    </div>
+  );
+}
+
+export default Profile;
