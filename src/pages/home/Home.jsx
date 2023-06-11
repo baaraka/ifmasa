976e4d53f44160b9d6794375a949909fb646c370
barakaganai <barakaganai01@gmail.com>
2023-06-11 18:02:07 +0300

    import components

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,11 @@
 import About from "../../components/about/About";
+import Consumption from "../../components/consumption/Consumption";
+import Footer from "../../components/footer/Footer";
 import Header from "../../components/header/Header";
 import Leader from "../../components/leader/Leader";
 import NavBar from "../../components/navBar/NavBar";
+import News from "../../components/news/News";
+import Product from "../../components/product/Product";
 import Profile from "../../components/profile/Profile";
 import "./Home.scss";
 
@@ -13,6 +17,10 @@ function Home() {
       <About />
       <Leader />
       <Profile />
+      <News />
+      <Product />
+      <Consumption />
+      <Footer />
     </div>
   );
 }
