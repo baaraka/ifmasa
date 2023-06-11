6b9ee6ea6bcc6133c67f7c667d592268ec40f427
barakaganai <barakaganai01@gmail.com>
2023-06-11 18:05:12 +0300

    create consumption.jsx

---
+++ src/components/consumption/Consumption.jsx
@@ -0,0 +1,200 @@
+import "./Consumption.scss";
+
+function Consumption() {
+  return (
+    <div className="Consumption">
+      <div className="consumptionContainer">
+        <h1>Investment Products for Public Consumption</h1>
+        <div className="consumptionContainerList">
+          <div className="consumptionContainerListItem">
+            <h2>Features</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+              repellendus dolore similique dolores perspiciatis tenetur corrupti
+              unde neque doloremque aperiam minus autem nisi est qui quam,
+              quidem, quisquam fugit soluta.
+            </p>
+            <h2>Benefits</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+              laborum animi excepturi libero assumenda autem perspiciatis, porro
+              labore distinctio eligendi, natus sint sapiente soluta optio
+              veniam impedit perferendis molestiae dignissimos!
+            </p>
+            <h2>target audience</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel,
+              perspiciatis illo tenetur veniam labore aspernatur optio
+              voluptatum quo, maiores, quia corrupti vero! Sequi optio cumque
+              facere ea velit saepe nam?
+            </p>
+          </div>
+          <div className="consumptionItem">
+            <img
+              src="https://images.pexels.com/photos/1139317/pexels-photo-1139317.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="consumptionContainerList">
+          <div className="consumptionItem">
+            <img
+              src="https://images.pexels.com/photos/1139317/pexels-photo-1139317.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+              alt=""
+            />
+          </div>
+          <div className="consumptionContainerListItem">
+            <h2>Features</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+              repellendus dolore similique dolores perspiciatis tenetur corrupti
+              unde neque doloremque aperiam minus autem nisi est qui quam,
+              quidem, quisquam fugit soluta.
+            </p>
+            <h2>Benefits</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+              laborum animi excepturi libero assumenda autem perspiciatis, porro
+              labore distinctio eligendi, natus sint sapiente soluta optio
+              veniam impedit perferendis molestiae dignissimos!
+            </p>
+            <h2>target audience</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel,
+              perspiciatis illo tenetur veniam labore aspernatur optio
+              voluptatum quo, maiores, quia corrupti vero! Sequi optio cumque
+              facere ea velit saepe nam?
+            </p>
+          </div>
+        </div>
+        <div className="consumptionContainerList">
+          <div className="consumptionContainerListItem">
+            <h2>Features</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+              repellendus dolore similique dolores perspiciatis tenetur corrupti
+              unde neque doloremque aperiam minus autem nisi est qui quam,
+              quidem, quisquam fugit soluta.
+            </p>
+            <h2>Benefits</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+              laborum animi excepturi libero assumenda autem perspiciatis, porro
+              labore distinctio eligendi, natus sint sapiente soluta optio
+              veniam impedit perferendis molestiae dignissimos!
+            </p>
+            <h2>target audience</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel,
+              perspiciatis illo tenetur veniam labore aspernatur optio
+              voluptatum quo, maiores, quia corrupti vero! Sequi optio cumque
+              facere ea velit saepe nam?
+            </p>
+          </div>
+          <div className="consumptionItem">
+            <img
+              src="https://images.pexels.com/photos/1139317/pexels-photo-1139317.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="consumptionContainerList">
+          <div className="consumptionItem">
+            <img
+              src="https://images.pexels.com/photos/1139317/pexels-photo-1139317.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+              alt=""
+            />
+          </div>
+          <div className="consumptionContainerListItem">
+            <h2>Features</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+              repellendus dolore similique dolores perspiciatis tenetur corrupti
+              unde neque doloremque aperiam minus autem nisi est qui quam,
+              quidem, quisquam fugit soluta.
+            </p>
+            <h2>Benefits</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+              laborum animi excepturi libero assumenda autem perspiciatis, porro
+              labore distinctio eligendi, natus sint sapiente soluta optio
+              veniam impedit perferendis molestiae dignissimos!
+            </p>
+            <h2>target audience</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel,
+              perspiciatis illo tenetur veniam labore aspernatur optio
+              voluptatum quo, maiores, quia corrupti vero! Sequi optio cumque
+              facere ea velit saepe nam?
+            </p>
+          </div>
+        </div>
+        <div className="consumptionContainerList">
+          <div className="consumptionContainerListItem">
+            <h2>Features</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+              repellendus dolore similique dolores perspiciatis tenetur corrupti
+              unde neque doloremque aperiam minus autem nisi est qui quam,
+              quidem, quisquam fugit soluta.
+            </p>
+            <h2>Benefits</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+              laborum animi excepturi libero assumenda autem perspiciatis, porro
+              labore distinctio eligendi, natus sint sapiente soluta optio
+              veniam impedit perferendis molestiae dignissimos!
+            </p>
+            <h2>target audience</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel,
+              perspiciatis illo tenetur veniam labore aspernatur optio
+              voluptatum quo, maiores, quia corrupti vero! Sequi optio cumque
+              facere ea velit saepe nam?
+            </p>
+          </div>
+          <div className="consumptionItem">
+            <img
+              src="https://images.pexels.com/photos/1139317/pexels-photo-1139317.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="consumptionContainerList">
+          <div className="consumptionItem">
+            <img
+              src="https://images.pexels.com/photos/1139317/pexels-photo-1139317.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+              alt=""
+            />
+          </div>
+          <div className="consumptionContainerListItem">
+            <h2>Features</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+              repellendus dolore similique dolores perspiciatis tenetur corrupti
+              unde neque doloremque aperiam minus autem nisi est qui quam,
+              quidem, quisquam fugit soluta.
+            </p>
+            <h2>Benefits</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+              laborum animi excepturi libero assumenda autem perspiciatis, porro
+              labore distinctio eligendi, natus sint sapiente soluta optio
+              veniam impedit perferendis molestiae dignissimos!
+            </p>
+            <h2>target audience</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel,
+              perspiciatis illo tenetur veniam labore aspernatur optio
+              voluptatum quo, maiores, quia corrupti vero! Sequi optio cumque
+              facere ea velit saepe nam?
+            </p>
+          </div>
+        </div>
+        <button>VIEW MORE</button>
+      </div>
+    </div>
+  );
+}
+
+export default Consumption;
