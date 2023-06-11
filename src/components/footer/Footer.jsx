8bda04803517d8759b019202b619af9de6cbeda4
barakaganai <barakaganai01@gmail.com>
2023-06-11 18:04:18 +0300

    create footer.jsx

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,56 @@
+import "./Footer.scss";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="footerContainer">
+        <hr />
+        <div className="footerContainerListItem">
+          <div className="fContainerList">
+            <h2>Categories</h2>
+            <span>IFMASA LEADERSHIP</span>
+            <span>IFMASA MEMBER'S PROFILE</span>
+            <span>IFMASA NEW LETTERS</span>
+            <span>IFMASA PRODUCTS</span>
+          </div>
+          <div className="fContainerList">
+            <h2>Categories</h2>
+            <span>IFMASA LEADERSHIP</span>
+            <span>IFMASA MEMBER'S PROFILE</span>
+            <span>IFMASA NEW LETTERS</span>
+            <span>IFMASA PRODUCTS</span>
+          </div>
+          <div className="fContainerList">
+            <h2>Categories</h2>
+            <span>IFMASA LEADERSHIP</span>
+            <span>IFMASA MEMBER'S PROFILE</span>
+            <span>IFMASA NEW LETTERS</span>
+            <span>IFMASA PRODUCTS</span>
+          </div>
+          <div className="fContainerList">
+            <h2>Categories</h2>
+            <span>IFMASA LEADERSHIP</span>
+            <span>IFMASA MEMBER'S PROFILE</span>
+            <span>IFMASA NEW LETTERS</span>
+            <span>IFMASA PRODUCTS</span>
+          </div>
+        </div>
+        <hr />
+        <div className="footerListItem">
+          <div className="footerItemList">
+            <h1>IFMASA</h1>
+            <span>copyright © 2023 IFMASA. All Right Reserved</span>
+          </div>
+          <div className="footerItemList">
+            <i class="iconFooter fa-brands fa-facebook"></i>
+            <i class="iconFooter fa-brands fa-twitter"></i>
+            <i class="iconFooter fa-brands fa-instagram"></i>
+            <i class="iconFooter fa-brands fa-linkedin"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
