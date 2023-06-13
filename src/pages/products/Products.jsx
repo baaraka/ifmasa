08d4c2b3d2b6972a9e243aa50564b66c12757128
barakaganai <barakaganai01@gmail.com>
2023-06-14 01:59:39 +0300

    create product.jsx page

---
+++ src/pages/products/Products.jsx
@@ -0,0 +1,220 @@
+import React from "react";
+import NavBar from "../../components/navBar/NavBar";
+import Footer from "../../components/footer/Footer";
+
+function Products() {
+  return (
+    <>
+      <NavBar />
+      <div className="product">
+        <div className="productContainer">
+          <h1>IFMASA PRODUCTS</h1>
+          <p>members books and publication</p>
+          <div className="productContainerList">
+            <div className="productContainerListItem">
+              <div className="containerImage">
+                <img
+                  src="https://images.pexels.com/photos/15959383/pexels-photo-15959383/free-photo-of-young-woman-in-a-blue-dress-tossing-her-mortarboard.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+                  alt=""
+                />
+                <div className="containerOverlay">
+                  <h2>Baraka Ganai</h2>
+                  <p>Summaries</p>
+                  <div className="rating">
+                    <span>
+                      <i className="star fa-solid fa-star"></i>
+                      <i className="star fa-solid fa-star"></i>
+                      <i className="star fa-solid fa-star"></i>
+                      <i className="star fa-solid fa-star"></i>
+                      <i className="star fa-solid fa-star"></i>
+                    </span>
+                    <p className="pStar">5</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="productContainerListItem">
+              <div className="containerImage">
+                <img
+                  src="https://images.pexels.com/photos/15959383/pexels-photo-15959383/free-photo-of-young-woman-in-a-blue-dress-tossing-her-mortarboard.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+                  alt=""
+                />
+                <div className="containerOverlay">
+                  <h2>Baraka Ganai</h2>
+                  <p>Summaries</p>
+                  <div className="rating">
+                    <span>
+                      <i className="star fa-solid fa-star"></i>
+                      <i className="star fa-solid fa-star"></i>
+                      <i className="star fa-solid fa-star"></i>
+                      <i className="star fa-solid fa-star"></i>
+                      <i className="star fa-solid fa-star"></i>
+                    </span>
+                    <p className="pStar">5</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="productContainerListItem">
+              <div className="containerImage">
+                <img
+                  src="https://images.pexels.com/photos/15959383/pexels-photo-15959383/free-photo-of-young-woman-in-a-blue-dress-tossing-her-mortarboard.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+                  alt=""
+                />
+                <div className="containerOverlay">
+                  <h2>Baraka Ganai</h2>
+                  <p>Summaries</p>
+                  <div className="rating">
+                    <span>
+                      <i className="star fa-solid fa-star"></i>
+                      <i className="star fa-solid fa-star"></i>
+                      <i className="star fa-solid fa-star"></i>
+                      <i className="star fa-solid fa-star"></i>
+                      <i className="star fa-solid fa-star"></i>
+                    </span>
+                    <p className="pStar">5</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="productContainerListItem">
+              <div className="containerImage">
+                <img
+                  src="https://images.pexels.com/photos/15959383/pexels-photo-15959383/free-photo-of-young-woman-in-a-blue-dress-tossing-her-mortarboard.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+                  alt=""
+                />
+                <div className="containerOverlay">
+                  <h2>Baraka Ganai</h2>
+                  <p>Summaries</p>
+                  <div className="rating">
+                    <span>
+                      <i className="star fa-solid fa-star"></i>
+                      <i className="star fa-solid fa-star"></i>
+                      <i className="star fa-solid fa-star"></i>
+                      <i className="star fa-solid fa-star"></i>
+                      <i className="star fa-solid fa-star"></i>
+                    </span>
+                    <p className="pStar">5</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="productContainerListItem">
+              <div className="containerImage">
+                <img
+                  src="https://images.pexels.com/photos/15959383/pexels-photo-15959383/free-photo-of-young-woman-in-a-blue-dress-tossing-her-mortarboard.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+                  alt=""
+                />
+                <div className="containerOverlay">
+                  <h2>Baraka Ganai</h2>
+                  <p>Summaries</p>
+                  <div className="rating">
+                    <span>
+                      <i className="star fa-solid fa-star"></i>
+                      <i className="star fa-solid fa-star"></i>
+                      <i className="star fa-solid fa-star"></i>
+                      <i className="star fa-solid fa-star"></i>
+                      <i className="star fa-solid fa-star"></i>
+                    </span>
+                    <p className="pStar">5</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="productContainerListItem">
+              <div className="containerImage">
+                <img
+                  src="https://images.pexels.com/photos/15959383/pexels-photo-15959383/free-photo-of-young-woman-in-a-blue-dress-tossing-her-mortarboard.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+                  alt=""
+                />
+                <div className="containerOverlay">
+                  <h2>Baraka Ganai</h2>
+                  <p>Summaries</p>
+                  <div className="rating">
+                    <span>
+                      <i className="star fa-solid fa-star"></i>
+                      <i className="star fa-solid fa-star"></i>
+                      <i className="star fa-solid fa-star"></i>
+                      <i className="star fa-solid fa-star"></i>
+                      <i className="star fa-solid fa-star"></i>
+                    </span>
+                    <p className="pStar">5</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="productContainerListItem">
+              <div className="containerImage">
+                <img
+                  src="https://images.pexels.com/photos/15959383/pexels-photo-15959383/free-photo-of-young-woman-in-a-blue-dress-tossing-her-mortarboard.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+                  alt=""
+                />
+                <div className="containerOverlay">
+                  <h2>Baraka Ganai</h2>
+                  <p>Summaries</p>
+                  <div className="rating">
+                    <span>
+                      <i className="star fa-solid fa-star"></i>
+                      <i className="star fa-solid fa-star"></i>
+                      <i className="star fa-solid fa-star"></i>
+                      <i className="star fa-solid fa-star"></i>
+                      <i className="star fa-solid fa-star"></i>
+                    </span>
+                    <p className="pStar">5</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="productContainerListItem">
+              <div className="containerImage">
+                <img
+                  src="https://images.pexels.com/photos/15959383/pexels-photo-15959383/free-photo-of-young-woman-in-a-blue-dress-tossing-her-mortarboard.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+                  alt=""
+                />
+                <div className="containerOverlay">
+                  <h2>Baraka Ganai</h2>
+                  <p>Summaries</p>
+                  <div className="rating">
+                    <span>
+                      <i className="star fa-solid fa-star"></i>
+                      <i className="star fa-solid fa-star"></i>
+                      <i className="star fa-solid fa-star"></i>
+                      <i className="star fa-solid fa-star"></i>
+                      <i className="star fa-solid fa-star"></i>
+                    </span>
+                    <p className="pStar">5</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="productContainerListItem">
+              <div className="containerImage">
+                <img
+                  src="https://images.pexels.com/photos/15959383/pexels-photo-15959383/free-photo-of-young-woman-in-a-blue-dress-tossing-her-mortarboard.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+                  alt=""
+                />
+                <div className="containerOverlay">
+                  <h2>Baraka Ganai</h2>
+                  <p>Summaries</p>
+                  <div className="rating">
+                    <span>
+                      <i className="star fa-solid fa-star"></i>
+                      <i className="star fa-solid fa-star"></i>
+                      <i className="star fa-solid fa-star"></i>
+                      <i className="star fa-solid fa-star"></i>
+                      <i className="star fa-solid fa-star"></i>
+                    </span>
+                    <p className="pStar">5</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export default Products;
