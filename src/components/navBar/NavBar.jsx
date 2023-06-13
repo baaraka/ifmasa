730c2bc67cd52c5ec96246285c5219b4df94dd2f
barakaganai <barakaganai01@gmail.com>
2023-06-14 02:02:25 +0300

    add link on navBar.jsx

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -23,32 +23,73 @@ function NavBar() {
     <div className={active || pathname !== "/" ? "navBar active" : "navBar"}>
       <div className="navContainer">
         <div className="left">
-          <h1>IFMASA</h1>
+          <Link to="/" className="link">
+            <h1>IFMASA</h1>
+          </Link>
         </div>
         <div className="middle">
-          <span>Home</span>
-          <span>About Us</span>
-          <span>Contact Us</span>
+          <Link to="/" className="link">
+            <span>Home</span>
+          </Link>
+          <Link to="/aboutUs" className="link">
+            <span>About Us</span>
+          </Link>
+          <Link to="/contactUs" className="link">
+            <span>Contact Us</span>
+          </Link>
         </div>
         <div className="right">
-          <button className="bUp">sign up</button>
-          <button className="bIn">sign in</button>
+          <Link to="/signUp">
+            <button className="bUp">sign up</button>
+          </Link>
+          <Link to="/signIn">
+            <button className="bIn">sign in</button>
+          </Link>
+        </div>
+        <div className="toggleMenu">
+          <i class="iconBars fa-solid fa-bars"></i>
+        </div>
+        <div className="sideBar">
+          <Link to="/" className="link">
+            <span>Home</span>
+          </Link>
+          <Link to="/aboutUs" className="link">
+            <span>About Us</span>
+          </Link>
+          <Link to="/contactUs" className="link">
+            <span>Contact Us</span>
+          </Link>
+          <Link to="/leaders" className="link">
+            <span className="menuSpan">IFMASA LEADERSHIP</span>
+          </Link>
+          <Link to="/members" className="link">
+            <span className="menuSpan">IFMASA MEMBER'S PROFILE</span>
+          </Link>
+          <span className="menuSpan">IFMASA NEWSLETTERS</span>
+          <Link to="/products" className="link">
+            <span className="menuSpan">IFMASA PRODUCTS</span>
+          </Link>
+          <Link to="/signUp">
+            <button className="bUp">sign up</button>
+          </Link>
+          <Link to="/signIn">
+            <button className="bIn">sign in</button>
+          </Link>
         </div>
       </div>
+
       {(active || pathname !== "/") && (
         <>
           <hr />
           <div className="menu">
-            <Link className="link">
+            <Link to="/leaders" className="link">
               <span className="menuSpan">IFMASA LEADERSHIP</span>
             </Link>
-            <Link className="link">
+            <Link to="/members" className="link">
               <span className="menuSpan">IFMASA MEMBER'S PROFILE</span>
             </Link>
-            <Link className="link">
-              <span className="menuSpan">IFMASA NEW LETTERS</span>
-            </Link>
-            <Link className="link">
+            <span className="menuSpan">IFMASA NEWSLETTERS</span>
+            <Link to="/products" className="link">
               <span className="menuSpan">IFMASA PRODUCTS</span>
             </Link>
           </div>
