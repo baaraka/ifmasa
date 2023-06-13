de4510f14da822e591ea9db109fd41e980300a00
barakaganai <barakaganai01@gmail.com>
2023-06-13 17:47:33 +0300

    implement images and content slide show

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,40 @@
+import { useEffect, useState } from "react";
 import "./Header.scss";
 
 function Header() {
+  const images = [
+    "https://images.pexels.com/photos/5553974/pexels-photo-5553974.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
+    "https://images.pexels.com/photos/10823334/pexels-photo-10823334.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
+    "https://images.pexels.com/photos/7972729/pexels-photo-7972729.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
+    "https://images.pexels.com/photos/1139317/pexels-photo-1139317.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
+    "https://images.pexels.com/photos/15005450/pexels-photo-15005450/free-photo-of-backview-of-a-female-graduate-in-academic-dress-holding-a-graduation-cap.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
+    "https://images.pexels.com/photos/5553642/pexels-photo-5553642.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
+    "https://images.pexels.com/photos/5859321/pexels-photo-5859321.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
+    "https://images.pexels.com/photos/7972737/pexels-photo-7972737.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
+    "https://images.pexels.com/photos/8106676/pexels-photo-8106676.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
+  ];
+
+  const [currentImage, setCurrentImage] = useState(0);
+
+  const goToNextImage = () => {
+    setCurrentImage((prev) => (prev + 1) % images.length);
+  };
+
+  const goToPreviousImage = () => {
+    setCurrentImage((prev) => {
+      if (prev === 0) {
+        return images.length - 1;
+      }
+      return prev - 1;
+    });
+  };
+
+  useEffect(() => {
+    const interval = setInterval(goToNextImage, 5000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="header">
       <div className="headerContainer">
@@ -17,10 +51,19 @@ function Header() {
           </p>
         </div>
         <div className="right">
-          <img
-            src="https://images.pexels.com/photos/4560148/pexels-photo-4560148.jpeg?auto=compress&cs=tinysrgb&w=800"
-            alt=""
-          />
+          <div className="iconSlider">
+            <i
+              className="iSlider fa-solid fa-chevron-left"
+              onClick={goToPreviousImage}
+            ></i>
+            <i
+              className="iSlider fa-solid fa-chevron-right"
+              onClick={goToNextImage}
+            ></i>
+          </div>
+          <div className="imageSlider">
+            <img src={images[currentImage]} alt="" />
+          </div>
         </div>
       </div>
     </div>
