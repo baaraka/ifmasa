a3bb81fdb49303860937bda14b6e0f4819fea39b
barakaganai <barakaganai01@gmail.com>
2023-06-14 02:01:15 +0300

    create contact page

---
+++ src/pages/contact/Contact.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Contacts from "../../components/contacts/Contacts";
+import NavBar from "../../components/navBar/NavBar";
+import Footer from "../../components/footer/Footer";
+
+function Contact() {
+  return (
+    <div>
+      <NavBar />
+      <Contacts />
+      <Footer />
+    </div>
+  );
+}
+
+export default Contact;
