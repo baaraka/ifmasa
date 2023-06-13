0ed35e20c9e5df5e6ec4563cdf907b2c57c73d3c
barakaganai <barakaganai01@gmail.com>
2023-06-14 02:04:14 +0300

    create aboutUs.jsx file

---
+++ src/components/aboutDiv/AboutDiv.jsx
@@ -0,0 +1,39 @@
+import "./AboutDiv.scss";
+
+function AboutDiv() {
+  return (
+    <div className="about">
+      <div className="aboutContainer">
+        <div className="left">
+          <img
+            src="https://images.pexels.com/photos/14509144/pexels-photo-14509144.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+            alt=""
+          />
+        </div>
+        <div className="right">
+          <h1>About Us</h1>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Recusandae, incidunt aliquid? Atque totam at doloremque non eos
+            nobis iure minima, placeat excepturi reiciendis exercitationem animi
+            nostrum officiis consectetur sit iusto. Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Eum tempore hic deleniti atque saepe
+            eligendi, aut provident. Similique officia quia possimus. Illum
+            magni dolorem impedit id nesciunt quidem unde quas. <br /> <br />
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae omnis
+            ad amet debitis rerum repudiandae nihil nemo non dolorum eaque
+            officia accusamus quam a, ratione est corrupti sint saepe similique!{" "}
+            <br />
+            <br />
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque
+            explicabo aliquid autem illum. Totam ratione dolorem, tenetur quis
+            accusantium ea, sunt vero ad blanditiis et modi molestiae at porro.
+            Quos.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AboutDiv;
