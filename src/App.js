1660fc9cf13d7eb741372e655934425317de3357
barakaganai <barakaganai01@gmail.com>
2023-06-14 02:04:48 +0300

    add page route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import "./App.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
+import AboutUs from "./pages/aboutUs/AboutUs";
+import Contact from "./pages/contact/Contact";
+import SignUp from "./pages/signUp/SignUp";
+import SignIn from "./pages/signIn/SignIn";
+import Leaders from "./pages/leaders/Leaders";
+import Members from "./pages/members/Members";
+import Products from "./pages/products/Products";
 
 function App() {
   return (
@@ -8,6 +15,13 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/aboutUs" element={<AboutUs />} />
+          <Route path="/contactUs" element={<Contact />} />
+          <Route path="/signUp" element={<SignUp />} />
+          <Route path="/signIn" element={<SignIn />} />
+          <Route path="/leaders" element={<Leaders />} />
+          <Route path="/members" element={<Members />} />
+          <Route path="/products" element={<Products />} />
         </Routes>
       </BrowserRouter>
     </div>
