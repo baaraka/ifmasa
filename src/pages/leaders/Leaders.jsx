81f21cd0081ad418cefc49f4afaaddf824e61d46
barakaganai <barakaganai01@gmail.com>
2023-06-14 02:00:13 +0300

    create leaders.jsx page

---
+++ src/pages/leaders/Leaders.jsx
@@ -0,0 +1,112 @@
+import React from "react";
+import NavBar from "../../components/navBar/NavBar";
+import Footer from "../../components/footer/Footer";
+
+function Leaders() {
+  return (
+    <>
+      {" "}
+      <NavBar />
+      <div className="leader">
+        <div className="leaderContainer">
+          <h1>IFMASA LEADERSHIP</h1>
+          <div className="lContainer">
+            <div className="lContainerList">
+              <h2>Leadership Philosophy</h2>
+              <p>
+                The leadership philosophy refers to the guiding principles,
+                values, and beliefs that shape how your organization's leaders
+                approach their roles and responsibilities. It outlines the
+                overall mindset and approach to leadership within your
+                organization
+              </p>
+            </div>
+            <div className="lContainerList">
+              <h2>Community Involvement</h2>
+              <p>
+                Community involvement refers to the initiatives and activities
+                your leadership team engages in to give back to the community or
+                support social causes. It showcases your organization's
+                commitment to making a positive impact beyond its core
+                operations.
+              </p>
+            </div>
+            <div className="lContainerList">
+              <h2>Future Vision</h2>
+              <p>
+                The future vision section outlines the long-term goals,
+                strategic direction, and growth plans of your organization as
+                envisioned by the leadership team. It provides insight into
+                where the organization is headed and the leadership team's
+                aspirations.s
+              </p>
+            </div>
+            <div className="lContainerList">
+              <h2>Future Vision</h2>
+              <p>
+                The future vision section outlines the long-term goals,
+                strategic direction, and growth plans of your organization as
+                envisioned by the leadership team. It provides insight into
+                where the organization is headed and the leadership team's
+                aspirations.s
+              </p>
+            </div>
+            <div className="lContainerList">
+              <h2>Future Vision</h2>
+              <p>
+                The future vision section outlines the long-term goals,
+                strategic direction, and growth plans of your organization as
+                envisioned by the leadership team. It provides insight into
+                where the organization is headed and the leadership team's
+                aspirations.s
+              </p>
+            </div>
+            <div className="lContainerList">
+              <h2>Future Vision</h2>
+              <p>
+                The future vision section outlines the long-term goals,
+                strategic direction, and growth plans of your organization as
+                envisioned by the leadership team. It provides insight into
+                where the organization is headed and the leadership team's
+                aspirations.s
+              </p>
+            </div>
+            <div className="lContainerList">
+              <h2>Future Vision</h2>
+              <p>
+                The future vision section outlines the long-term goals,
+                strategic direction, and growth plans of your organization as
+                envisioned by the leadership team. It provides insight into
+                where the organization is headed and the leadership team's
+                aspirations.s
+              </p>
+            </div>
+            <div className="lContainerList">
+              <h2>Future Vision</h2>
+              <p>
+                The future vision section outlines the long-term goals,
+                strategic direction, and growth plans of your organization as
+                envisioned by the leadership team. It provides insight into
+                where the organization is headed and the leadership team's
+                aspirations.s
+              </p>
+            </div>
+            <div className="lContainerList">
+              <h2>Future Vision</h2>
+              <p>
+                The future vision section outlines the long-term goals,
+                strategic direction, and growth plans of your organization as
+                envisioned by the leadership team. It provides insight into
+                where the organization is headed and the leadership team's
+                aspirations.s
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export default Leaders;
