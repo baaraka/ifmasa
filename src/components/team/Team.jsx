f0472bd9f3bee8cd3f26f7b1b10cb12763892650
barakaganai <barakaganai01@gmail.com>
2023-06-14 02:02:09 +0300

    create team.jsx file

---
+++ src/components/team/Team.jsx
@@ -0,0 +1,27 @@
+import "./Team.scss";
+
+function Team() {
+  return (
+    <div className="team">
+      <div className="teamContainer">
+        <div className="left">
+          <h1>Our Team</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
+            ratione numquam optio aperiam facere. Placeat facere architecto
+            saepe inventore. Placeat dolorum non vero alias adipisci, accusamus
+            dolores? Dolore, hic consectetur!
+          </p>
+        </div>
+        <div className="right">
+          <img
+            src="https://images.pexels.com/photos/5553080/pexels-photo-5553080.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Team;
