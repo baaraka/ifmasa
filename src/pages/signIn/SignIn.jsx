917f8b97ef5537e94dc98119708a510908990da7
barakaganai <barakaganai01@gmail.com>
2023-06-15 17:48:21 +0300

    implement sign up logic

--- src/pages/signIn/SignIn.jsx
+++ src/pages/signIn/SignIn.jsx
@@ -1,37 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import NavBar from "../../components/navBar/NavBar";
+import Footer from "../../components/footer/Footer";
 
 function SignIn() {
+  const [loginStatus, setLoginStatus] = useState("");
+
+  const handleSignIn = (event) => {
+    event.preventDefault();
+
+    // Get form values
+    const username = event.target.elements.username.value;
+    const password = event.target.elements.password.value;
+
+    // Retrieve user data from localStorage
+    const user = JSON.parse(localStorage.getItem("user"));
+
+    // Check if user exists and credentials match
+    if (user && user.username === username && user.password === password) {
+      // Perform login actions (e.g., set authentication status, redirect, etc.)
+      setLoginStatus("success");
+      setTimeout(() => {
+        setLoginStatus(""); // Clear login status after 2 seconds
+      }, 2000);
+    } else {
+      setLoginStatus("failure");
+      setTimeout(() => {
+        setLoginStatus(""); // Clear login status after 2 seconds
+      }, 2000);
+    }
+
+    // Clear form values
+    event.target.reset();
+  };
   return (
-    <div className="signUp">
-      <div className="signUpContainer">
-        <div className="left">
-          <img
-            src="https://images.pexels.com/photos/4560142/pexels-photo-4560142.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
-            alt=""
-          />
-        </div>
-        <div className="right">
-          <form>
-            <div className="labelDiv">
-              <label>Username</label>
-              <input type="text" />
-            </div>
-            <div className="labelDiv">
-              <label>Password</label>
-              <input type="password" />
-            </div>
-            <button>Sign In</button>
-            <p>
-              You don't have account?{" "}
-              <Link to="/signUp">
-                <button>Sign Up</button>
-              </Link>
-            </p>
-          </form>
+    <>
+      <NavBar />
+      <div className="signUp">
+        <div className="signUpContainer">
+          <div className="left">
+            <img
+              src="https://images.pexels.com/photos/4560142/pexels-photo-4560142.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+              alt=""
+            />
+          </div>
+          <div className="right">
+            <form onSubmit={handleSignIn}>
+              <div className="labelDiv">
+                <label>Username</label>
+                <input type="text" name="username" required />
+              </div>
+              <div className="labelDiv">
+                <label>Password</label>
+                <input type="password" name="password" required />
+              </div>
+              <button type="submit">Sign In</button>
+              {loginStatus === "success" && <p>Login successful!</p>}
+              {loginStatus === "failure" && (
+                <p>Invalid email or password. Please try again.</p>
+              )}
+              <p>
+                Don't have an account?{" "}
+                <Link to="/signUp">
+                  <button type="submit">Sign Up</button>
+                </Link>
+              </p>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 }
 
